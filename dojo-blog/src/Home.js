f820a911775a4f0fe7bdb82ca328188fb6585f19
Sam <Samuel.pellot001@gmail.com>
2024-03-25 16:27:20 -0400

    Update script at home

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -1,29 +1,61 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
-  const [blogs, setBlogs] = useState([
-    { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-    { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-    { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
-  ])
+  const [blogs, setBlogs] = useState(null); 
 
   const [name, setName] = useState('mario');
 
-  const handleDelete = (id) => {
-    const newBlogs = blogs.filter(blog => blog.id !== id);
-    setBlogs(newBlogs);
-  }
+  const [error, setError] = useState(null);
 
-  useEffect(() => {
+  const [isPending, setIsPending] = useState(true);
+  //when fetching data, pending. When fetched, false. No longer pending.
+
+
+  // const handleDelete = (id) => {
+  //   const newBlogs = blogs.filter(blog => blog.id !== id);
+  //   setBlogs(newBlogs);
+  // }
+
+  /*{useEffect(() => {
     console.log('use effect ran');
     console.log(blogs);
-  }, [name])
+  }, [name])}*/
+
+  useEffect(() => {
+    setTimeout(() => {
+        fetch('http://localhost:8000/blog')
+      .then(res =>{
+        console.log(res);
+        if (!res.ok)
+        {
+          throw Error("Could not fetch data...");
+        }
+        return res.json();
+      })
+      .then(data => {
+        console.log(data);
+        setBlogs(data);
+        setIsPending(false);
+        setError(null);
+      }) /*{} returns to us a promise. get response object, then data*/
+      .catch((e) => {
+        setIsPending(false);
+        setError(e.message);
+        //console.log(e.message);
+      }) /*catch a network error if it happens*/
+    }, 1000)
+  }, []); 
+  // above is a dependancy array for useEffect.
 
   return (
     <div className="home">
-      <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
-      <button onClick={() => setName('luigi')}>change name</button>
+      {error && <div>{error}</div>}
+      {isPending && <div>Loading...</div> }
+       {blogs && <BlogList blogs={blogs} title="All Blogs" /> } 
+       {/*{ handleDelete={handleDelete} }*/}
+       {/* we want to dynamically tag before the above line runs, we dont want mapping to happen when the data is still null */}
+      <button onClick={() => setName('luigi')}>change name</button> 
     </div>
   );
 }
